59083d74ac772533f55a4d4d970964d2adfd25c7
TheCodingLeo <114520101+TheCodingLeo@users.noreply.github.com>
2023-10-18 21:23:29 -0400

    grenade changes

--- Assets/Scripts/grenade.cs
+++ Assets/Scripts/grenade.cs
@@ -8,17 +8,40 @@ public class grenade : MonoBehaviour
     [SerializeField] int speed;
     [SerializeField] int destroyTime;
     [SerializeField] GameObject explosion;
+    public GameObject player;
 
     void Start()
     {
         //rb.velocity = (gameManager.instance.player.transform.position - transform.position).normalized * speed;
         StartCoroutine(explode());
     }
-
+    void Update()
+    {
+        if (Input.GetButtonDown("g"))
+        {
+            ThrowGrenade();
+        }
+    }
     IEnumerator explode()
     {
         yield return new WaitForSeconds(destroyTime);
         Instantiate(explosion, transform.position, explosion.transform.rotation);
         Destroy(gameObject);
+    }
+    void ThrowGrenade()
+    {
+        // Calculate the player's position and direction
+        Vector3 playerPosition = player.transform.position;
+        Vector3 throwDirection = (playerPosition - transform.position).normalized;
+
+        // Create a new instance of the grenade
+        GameObject grenadeInstance = Instantiate(gameObject, transform.position, transform.rotation);
+
+        // Access the Rigidbody component of the grenade and apply velocity
+        Rigidbody grenadeRigidbody = grenadeInstance.GetComponent<Rigidbody>();
+        grenadeRigidbody.velocity = throwDirection * speed;
+
+        // Start the countdown for explosion
+        grenadeInstance.GetComponent<grenade>().StartCoroutine(grenadeInstance.GetComponent<grenade>().explode());
     }
 }
