f1aec960e37f091e746c35010f27a38fede26007
john sauve <116692200+JohnSauve@users.noreply.github.com>
2023-10-29 21:29:44 -0400

    added prefabs and modified to scripts

--- Assets/Scripts/basicObjectMovement.cs
+++ Assets/Scripts/basicObjectMovement.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class basicObjectMovement : MonoBehaviour
 {
-    [SerializeField] int speed;
+    [SerializeField] float speed;
     [SerializeField] float destroyTime;
     [SerializeField] int damage;
+    [SerializeField] float pingPongDis;
     [SerializeField] bool damageIT;
+    [SerializeField] bool pingPong;
+
+    private Vector3 pointA;
+    private Vector3 pointB;
 
     //public simpleObjectSpawner orgin;
+    private void Start()
+    {
+        pointA = transform.position;
+        pointB = new Vector3(transform.position.x + pingPongDis, transform.position.y, transform.position.z);
+    }
 
-    
     void Update()
     {
+        if (pingPong == true)
+        {
+            float time = Mathf.PingPong(Time.time * speed, 1);
+            transform.position = Vector3.Lerp(pointA, pointB, time);
+        }
+        else
+        {
             transform.position += transform.forward * speed * Time.deltaTime;
             StartCoroutine(Destroy());
+        }
     }
 
     IEnumerator Destroy()
