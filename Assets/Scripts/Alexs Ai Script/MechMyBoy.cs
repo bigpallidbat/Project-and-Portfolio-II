81c3f97c001d430bc7d5c894aa58459c6d848c66
bigpallidbat <82740920+bigpallidbat@users.noreply.github.com>
2023-11-06 11:13:49 -0700

    spinning in place fixed

--- Assets/Scripts/Alexs Ai Script/MechMyBoy.cs
+++ Assets/Scripts/Alexs Ai Script/MechMyBoy.cs
@@ -141,7 +141,7 @@ public class MechMyBoy : MonoBehaviour, IDamage
 
     IEnumerator Roam()
     {
-        if (agent.remainingDistance < 0.1f && !destinationChosen && !isAttacking)
+        if (agent.remainingDistance < 0.5f && !destinationChosen && !isAttacking)
         {
             destinationChosen = true;
             agent.stoppingDistance = 0;
