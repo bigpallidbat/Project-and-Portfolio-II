763d79c498e6e9efe782fc251eefb160d070fa90
bigpallidbat <82740920+bigpallidbat@users.noreply.github.com>
2023-11-08 12:49:37 -0700

    Revert "push 1 broke girl and new anim(far from done)"
    This reverts commit 063bd650a661b1742ce7c45bcf879c7d431483f3.

--- Assets/Scripts/specialEnemyAi.cs
+++ Assets/Scripts/specialEnemyAi.cs
@@ -1,28 +1,10 @@
 using System.Collections;
-using UnityEditor.TerrainTools;
 using UnityEngine;
 using UnityEngine.AI;
-//using static UnityEngine.RuleTile.TilingRuleOutput;
-using static UnityEngine.UI.Image;
 
 public class specialEnemyAi : MonoBehaviour, IDamage
 {
     [Header("----- Components -----")]
-    [SerializeField] AudioSource soundSFX;
-    [SerializeField] AudioClip painSound;
-    [Range(0, 1)][SerializeField] float audPainVol;
-    [SerializeField] AudioClip deathSound;
-    [Range(0, 1)][SerializeField] float audDeathVol;
-    [SerializeField] AudioClip attckSound;
-    [Range(0, 1)][SerializeField] float audAttackVol;
-    [SerializeField] AudioClip VpainSound;
-    [Range(0, 1)][SerializeField] float audVpainVol;
-    [SerializeField] AudioClip VdeathSound;
-    [Range(0, 1)][SerializeField] float audVdeathVol;
-    [SerializeField] AudioClip seeSound;
-    [Range(0, 1)][SerializeField] float audSeeVol;
-    [SerializeField] AudioClip woosh;
-    [Range(0, 1)][SerializeField] float audWooshVol;
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform shootPos;
@@ -32,44 +14,31 @@ public class specialEnemyAi : MonoBehaviour, IDamage
     [SerializeField] Transform LeftHand;
     [SerializeField] Transform RightHand;
     [SerializeField] ParticleSystem deathSystem;
-    [SerializeField] Collider damageCOL;
-    [SerializeField] SkinnedMeshRenderer mainBody;
-    [SerializeField] GameObject VoxelDamage;
-    [SerializeField] GameObject DeathOBJ;
 
     [Header("----- Stats -----")]
     [SerializeField] int HP;
     [SerializeField] int targetFaceSpeed;
-    [SerializeField] float meleeRange;
     [Range(30, 180)][SerializeField] int viewAngle;
 
-    [Header("----- Attack Stats -----")]
+    [Header("----- Gun Stats -----")]
     [SerializeField] GameObject bullet;
     [SerializeField] float fireRate;
     [SerializeField] int gunDamage;
     [SerializeField] int bulletSpeed;
-    [SerializeField] float bulletLifeSpan;
-    [SerializeField] Collider hitBoxCOL;
     [Range(30, 180)][SerializeField] int shootAngle;
 
     float angleToPlayer;
-    //Vector3 PlayerDir;
     Vector3 playerDir;
-    Vector3 StartingPos;
     const int numDamAnims = 3;
     int lastAnim = -1;
-    bool isAttacking;
+    bool isShooting;
     bool playerInRange;
     bool isMoving;
-    bool inPain;
-    int diceroll;
 
     // Start is called before the first frame update
     void Start()
     {
-        bullet.GetComponent<Bullet>().DestroyTime = bulletLifeSpan;
-        soundSFX = GetComponent<AudioSource>();
-        StartingPos = transform.position;
+
         setModel();
 
     }
@@ -82,45 +51,10 @@ public class specialEnemyAi : MonoBehaviour, IDamage
         {
 
         }
-        //moving();
-
-        if (agent.isActiveAndEnabled)
-        {
-            anim.SetFloat("speed", agent.velocity.normalized.magnitude);
-            if (playerInRange && CanSeePlayer() && !inPain) { }
-            else if (inPain) agent.SetDestination(transform.position);
-        }
-
+        moving();
+        
     }
-    bool CanSeePlayer()
-    {
-        Vector3 playerPosition = gameManager.Instance.player.transform.position;
-        Vector3 directionToPlayer = playerPosition - headPos.position;
-        float angleToPlayer = Vector3.Angle(directionToPlayer, transform.forward);
-        float playerDist = Vector3.Distance(playerPosition, transform.position);
 
-        RaycastHit hit;
-
-        Vector3 raycastOrigin = headPos.position + Vector3.up * 0.5f;
-
-        if (Physics.Raycast(raycastOrigin, directionToPlayer, out hit))
-        {
-            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewAngle)
-            {
-                agent.SetDestination(playerPosition);
-                if (agent.remainingDistance < agent.stoppingDistance)
-                    faceTarget();
-
-                if (angleToPlayer <= shootAngle && !isAttacking)
-                {
-                    if (playerDist <= meleeRange) StartCoroutine(meleeAttack());
-                    else  StartCoroutine(shoot());
-                }
-                return true;
-            }
-        }
-        return false;
-    }
     void setModel()
     {
         Gun = Instantiate(Gun);
@@ -130,70 +64,21 @@ public class specialEnemyAi : MonoBehaviour, IDamage
         shootPos = Gun.transform.GetChild(0);
     }
 
-    //public void takeDamage(int damage)
-    //{
-    //    HP -= damage;
-    //    StartCoroutine(flashDamage());
-    //    agent.SetDestination(gameManager.Instance.player.transform.position);
-    //    if (HP <= 0)
-    //    {
-    //        anim.SetTrigger("Death");
-    //        gameManager.Instance.updateGameGoal();
-    //        StopAllCoroutines();
-    //        Instantiate(deathSystem, transform.position, transform.rotation);
-    //        StartCoroutine(death());
-    //    }
-    //}
-    public void takeDamage(int amount)
+    public void takeDamage(int damage)
     {
-        HP -= amount;
-        hitBoxCOL.enabled = false;
-        soundSFX.PlayOneShot(VpainSound, audVpainVol);
-        soundSFX.PlayOneShot(painSound, audPainVol);
-
+        HP -= damage;
+        StartCoroutine(flashDamage());
+        agent.SetDestination(gameManager.Instance.player.transform.position);
         if (HP <= 0)
         {
-            anim.SetBool("dead", true);
+            anim.SetTrigger("Death");
+            gameManager.Instance.updateGameGoal();
             StopAllCoroutines();
-            faceTarget();
-            soundSFX.PlayOneShot(VdeathSound, audVdeathVol);
-            soundSFX.PlayOneShot(deathSound, audDeathVol);
-            mainBody.enabled = false;
-            VoxelDamage.gameObject.SetActive(false);
-            DeathOBJ.gameObject.SetActive(true);
-            agent.enabled = false;
-            damageCOL.enabled = false;
-            Quaternion Rot = Quaternion.LookRotation(playerDir);
-            transform.rotation = Rot;
+            Instantiate(deathSystem, transform.position, transform.rotation);
+            StartCoroutine(death());
         }
-        else
-            StartCoroutine(FlashDamage());
-
     }
-    public void pop()
-    {
-        StartCoroutine(Pop());
-    }
-    IEnumerator Pop()
-    {
-
-        yield return new WaitForSeconds(1);
-    }
-    IEnumerator FlashDamage()
-    {
-        inPain = true;
-        mainBody.enabled = false;
-        VoxelDamage.gameObject.SetActive(true);
-        agent.SetDestination(transform.position);
-        yield return new WaitForSeconds(0.085714f);
-        VoxelDamage.gameObject.SetActive(false);
-        mainBody.enabled = true;
-        diceroll = Random.Range(0, 12);
-        if (diceroll < HP) anim.SetTrigger("pain");
-        else inPain = false;
-
-    }
-
+    
     IEnumerator death()
     {
         yield return new WaitForSeconds(5);
@@ -216,20 +101,15 @@ public class specialEnemyAi : MonoBehaviour, IDamage
 
     IEnumerator shoot()
     {
-        isAttacking = true;
+        isShooting = true;
         bullet.GetComponent<Bullet>().speed = bulletSpeed;
         bullet.GetComponent<Bullet>().damage = gunDamage;
         //bScript.damage = shootDamage;
         //bScript.speed = bulletSpeed;
-        //shootPos.transform.rotation = Quaternion.LookRotation(playerDir);
-       // Instantiate(bullet, shootPos.position, transform.rotation);
+        shootPos.transform.rotation = Quaternion.LookRotation(playerDir);
+        Instantiate(bullet, shootPos.position, transform.rotation);
         yield return new WaitForSeconds(fireRate);
-        isAttacking = false;
-    }
-
-    IEnumerator meleeAttack()
-    {
-        yield return null;
+        isShooting = false;
     }
 
     void faceTarget()
@@ -256,7 +136,7 @@ public class specialEnemyAi : MonoBehaviour, IDamage
 
     void moving()
     {
-        if (agent.velocity != Vector3.zero)
+        if(agent.velocity != Vector3.zero)
         {
             isMoving = true;
             anim.SetBool("Aiming", !isMoving);
@@ -285,10 +165,10 @@ public class specialEnemyAi : MonoBehaviour, IDamage
                     faceTarget();
                     anim.SetFloat("Speed", 0);
                     anim.SetBool("Aiming", true);
-                    if (angleToPlayer <= shootAngle && !isAttacking)
+                    if (angleToPlayer <= shootAngle && !isShooting)
                         StartCoroutine(shoot());
                 }
-
+                    
 
 
                 return true;
