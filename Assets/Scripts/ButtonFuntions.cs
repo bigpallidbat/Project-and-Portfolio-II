17bda1f02a39de4d154ed765010eb97936a8c110
Chance Clark <chanceclark335@yahoo.com>
2023-11-12 22:45:48 -0600

    Final Fixes

--- Assets/Scripts/ButtonFuntions.cs
+++ Assets/Scripts/ButtonFuntions.cs
@@ -10,21 +10,25 @@ public class ButtonFuntions : MonoBehaviour
     public void resume()
     {
         gameManager.Instance.stateUnpause();
+        gameManager.Instance.playSFX();
     }
     public void restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         gameManager.Instance.stateUnpause();
+        gameManager.Instance.playSFX();
     }
     public void quit()
     {
         Application.Quit();
+        gameManager.Instance.playSFX();
     }
 
     public void respawnPlayer()
     {
         gameManager.Instance.stateUnpause();
         gameManager.Instance.playerScript.spawnPlayerminor();
+        gameManager.Instance.playSFX();
     }
 
     public void mainMenu()
@@ -32,18 +36,21 @@ public class ButtonFuntions : MonoBehaviour
         SceneManager.LoadScene(0);
         gameManager.currentlevel = gameManager.Levels.MainMenu;
         gameManager.Instance.stateUnpauseWithCursor();
+        gameManager.Instance.playSFX();
 
     }
     public void startGame()
     {
         sceneManager.Instance.nextScene(1);
         screenManager.Instance.turnOffScreens();
+        gameManager.Instance.playSFX();
     }
 
 
     public void levelButton()
     {
         screenManager.Instance.setScreen(2);
+        gameManager.Instance.playSFX();
     }
 
     public void levelSelect(int level)
@@ -62,20 +69,24 @@ public class ButtonFuntions : MonoBehaviour
     public void Back()
     {
         screenManager.Instance.setScreen(1);
+        gameManager.Instance.playSFX();
     }
 
     public void settingsButton()
     {
         screenManager.Instance.setScreen(3);
+        gameManager.Instance.playSFX();
     }
 
     public void Controlsbutton()
     {
         screenManager.Instance.setScreen(4);
+        gameManager.Instance.playSFX();
     }
 
     public void BackControls()
     {
         screenManager.Instance.setScreen(3);
+        gameManager.Instance.playSFX();
     }
 }
